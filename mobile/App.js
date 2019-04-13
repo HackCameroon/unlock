69e2941bca7bf45923ebcb1836d5d15f3d355359
eminguyen <emilyhuongnguyen@gmail.com>
2019-04-13 16:20:55 -0700

    Add user's current location

--- mobile/App.js
+++ mobile/App.js
@@ -67,8 +67,8 @@ export default class App extends React.Component {
     }
 
     navigator.geolocation.getCurrentPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
+      let lat = parseFloat(position.coords.latitude)
+      let long = parseFloat(position.coords.longitude)
 
       this.setState({
         coords: [],
@@ -88,13 +88,14 @@ export default class App extends React.Component {
     try {
       let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }&key=AIzaSyBFb-ijeIhz4nmGHusKruKaizPOYDsVipU`)
         let respJson = await resp.json();
+        console.log(respJson.routes[0]);
         let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
 
         let coords = points.map((point, index) => {
-            return  {
-                latitude : point[0],
-                longitude : point[1]
-            }
+          return  {
+            latitude : point[0],
+            longitude : point[1]
+          }
         })
 
         this.setState({
